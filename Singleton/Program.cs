f3151622da37f2814e701adcc23847d578115b82
luckyDucky3 <maxlebed04@mail.ru>
2025-02-14 14:58:00 +0300

    Add singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Singleton;
+
+public class Engine
+{
+    private static Engine? _instance;
+
+    public string Name { get; }
+
+    private Engine(string name)
+    {
+        Name = name;
+    }
+    
+    public static Engine GetInstance(string name)
+    {
+        return _instance ??= new Engine(name);
+    }
+    
+}
+
+public class Car
+{
+    public Engine? Instance;
+
+    public void Collect(string name)
+    {
+        Instance = Engine.GetInstance(name);
+    }
+    
+}
+
+internal static class Program
+{
+    static void Main()
+    {
+        Car car = new Car();
+        car.Collect("Двигатель v8.1");
+        Console.WriteLine(car.Instance?.Name);
+        
+        car.Collect("Двигатель v12.0");
+        Console.WriteLine(car.Instance?.Name);
+    }
+}
