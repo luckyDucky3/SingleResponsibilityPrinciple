703419cfe9e2e6af4714b0152a71093ab0bea5ce
luckyDucky3 <maxlebed04@mail.ru>
2025-02-21 02:54:06 +0300

    lazy loading

---
+++ Lazy loading/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Lazy_loading;
+
+public class Configuration
+{
+    private Dictionary<string, string> PrimarySettings { get; set; }
+    private readonly Lazy<SecondarySettings> _optionalSettings = new(() => new SecondarySettings());
+    public Configuration()
+    {
+        Console.WriteLine("Loading configuration...");
+        PrimarySettings = new Dictionary<string, string>
+        {
+            { "Theme", "Dark" },
+            { "Language", "English" },
+            { "Notifications", "Enabled" }
+        };
+        Console.WriteLine("Configuration loaded!");
+    }
+
+    public string? GetPrimarySetting(string key)
+    {
+        return PrimarySettings.GetValueOrDefault(key);
+    }
+    
+    public void SetSecondarySettings(Dictionary<string, string> settings)
+    {
+        _optionalSettings.Value.SecondarySettingsDictionary = new Dictionary<string, string>(settings);
+    }
+    
+    public string? GetSecondarySetting(string key)
+    {
+        return _optionalSettings.Value.SecondarySettingsDictionary.GetValueOrDefault(key);
+    }
+
+    private class SecondarySettings
+    {
+        public Dictionary<string, string> SecondarySettingsDictionary { get; set; }
+        public SecondarySettings()
+        {
+            Thread.Sleep(5000);
+            SecondarySettingsDictionary = new Dictionary<string, string>();
+        }
+    }
+}
+
+public class Application
+{
+    private readonly Configuration _config  = new();
+
+    public void Run()
+    {
+        Console.WriteLine("Приложение запущено");
+
+        Console.WriteLine("Использование конфигурации:");
+        Console.WriteLine($"Theme: {_config.GetPrimarySetting("Theme")}");
+        Console.WriteLine($"Language: {_config.GetPrimarySetting("Language")}");
+    }
+}
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var app = new Application();
+        app.Run();
+    }
+}
---
+++ Lazy/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Lazy;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
